a4e4df2cbb2373cb3511695ec5eee8b26a0eab91
Huajie Qian <hq2157@columbia.edu>
2024-06-17 13:26:58 -0700

    update

--- Examples.py
+++ Examples.py
@@ -2,6 +2,8 @@ from Bagging import BAG, ReBAG
 from sklearn.linear_model import LinearRegression
 import numpy as np
 from numpy.typing import NDArray
+import cvxpy as cp
+from typing import Union
 
 
 
@@ -12,6 +14,34 @@ class BagBinary(BAG):
     
     def isIdentical(self, result1: int, result2: int) -> bool:
         return result1 == result2
+    
+    def genSample(self, n: int, rng: np.random.Generator) -> NDArray:
+        return np.hstack((rng.normal(loc = 0.1, size = (n, 1)), rng.normal(loc = 0.0, size = (n, 1))))
+    
+
+class BagLP(BAG):
+    def __init__(self, c: NDArray, A: NDArray, b: NDArray, lb: NDArray, ub: NDArray, numParallelTrain: int = 1, randomState: Union[np.random.Generator, int, None] = None):
+        super().__init__(numParallelTrain, randomState)
+        self._c: NDArray = np.asarray(c)
+        self._A: NDArray = np.asarray(A)
+        self._b: NDArray = np.asarray(b)
+        self._lb: NDArray = np.asarray(lb)
+        self._ub: NDArray = np.asarray(ub)
+    
+    def train(self, sample: NDArray) -> NDArray:
+        x = cp.Variable(len(self._c))
+        prob = cp.Problem(cp.Minimize(np.mean(sample, axis = 0) @ x),
+                          [self._A @ x <= self._b, x >= self._lb, x <= self._ub])
+        prob.solve()
+
+        if prob.status == "optimal":
+            return x.value
+    
+    def isIdentical(self, result1: NDArray, result2: NDArray) -> bool:
+        return np.max(np.abs(result1 - result2)) < 1e-6
+    
+    def genSample(self, n: int, rng: np.random.Generator) -> NDArray:
+        return rng.normal(loc = self._c, size = (n, len(self._c)))
 
 
 class ReBagLR(ReBAG):
@@ -32,3 +62,10 @@ class ReBagLR(ReBAG):
     def evaluate(self, trainingResult: LinearRegression, sample: NDArray) -> float:
         error = trainingResult.predict(sample[:, 1:]) - sample[:, 0]
         return np.mean(error ** 2)
+    
+    def genSample(self, n: int, rng: np.random.Generator) -> NDArray:
+        d = 10
+        XX = rng.normal(size = (n, d))
+        beta = np.linspace(0, 1, num = d)
+        y: NDArray = np.dot(XX, beta) + rng.normal(size = n)
+        return np.hstack((y.reshape(-1, 1), XX))
--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-from Examples import BagBinary, ReBagLR
+from Examples import BagBinary, BagLP, ReBagLR
 import numpy as np
 from multiprocessing import set_start_method
 import os
@@ -7,30 +7,32 @@ import time
 
 
 if __name__ == "__main__":
-    rngData = np.random.default_rng(seed = 888)
-    N = 10000
-    d = 10
-    XX = rngData.normal(size = (N, d))
-    beta = np.linspace(0, 1, num = d)
-    y: np.ndarray = np.dot(XX, beta) + rngData.normal(size = N)
-
-    sample = np.hstack((y.reshape(-1, 1), XX))
-
     if os.name == "posix":
         set_start_method("fork")
+        
+    rngData = np.random.default_rng(seed = 888)
+
+    binary = BagBinary(numParallelTrain = 4, randomState = 666)
+    sample = binary.genSample(10000, rngData)
+    tic = time.time()
+    output = binary.run(sample, 1000, 200)
+    print(f"BaggedBinary took {time.time() - tic} secs, result: ", output)
 
     lr = ReBagLR(False, numParallelTrain = 4, numParallelEval = 4, randomState = 666)
+    sample = lr.genSample(10000, rngData)
     tic = time.time()
     output = lr.run(sample, 1000, 1000, 100, 200)
     print(f"ReBaggedLR took {time.time() - tic} secs, result: ", output.coef_)
 
-    lr.resetRandomState()
+    d = 10
+    lb = np.zeros(d)
+    ub = np.full(d, np.inf)
+    rngLP = np.random.default_rng(seed=999)
+    A = rngLP.uniform(low=0, high=1, size=(20, d))
+    b = 10 * np.ones(20)
+    c = rngLP.uniform(low=0, high=1, size=d)
+    lp = BagLP(c, A, b, lb, ub, numParallelTrain = 4, randomState = 666)
+    sample = lp.genSample(10000, rngData)
     tic = time.time()
-    output = lr.run(sample, 1000, 1000, 100, 200)
-    print(f"ReBaggedLR took {time.time() - tic} secs, result: ", output.coef_)
-
-    binarySample = np.hstack((rngData.normal(loc = 0.1, size = (N, 1)), rngData.normal(loc = 0.0, size = (N, 1))))
-    binary = BagBinary(numParallelTrain = 4, randomState = 666)
-    tic = time.time()
-    output = binary.run(binarySample, 1000, 200)
-    print(f"BaggedBinary took {time.time() - tic} secs, result: ", output)
+    output = lp.run(sample, 1000, 200)
+    print(f"BaggedLP took {time.time() - tic} secs, result: ", output)
